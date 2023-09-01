e85383c40697396dfa83c3250bd974d66a969100
adityantamarapu <66235725+adityantamarapu@users.noreply.github.com>
2023-09-01 17:54:00 +0530

    Updated multi_play_game.js
    Allows two players play

--- TalentSprint/javascript/multi_play_game.js
+++ TalentSprint/javascript/multi_play_game.js
@@ -11,39 +11,10 @@ let applePosition = { x: -1, y: -1 }; // Initial apple position
 
 // multiplayer battle code
 const urlParams = new URLSearchParams(window.location.search);
-const device2IP = urlParams.get("2IP");
+const device1IP = urlParams.get("2IP");
 
-if (device2IP == null) {
-  const WebSocket = require("ws");
-  const server = new WebSocket.Server({ port: 8080 });
-
-  let playerSegmentsOfPlayer2 = []; // Array to store playerSegments of the other player
-
-  server.on("connection", (socket) => {
-    console.log("Device 1: Connection established with Device 2");
-
-    // Send playerSegments data to Device 2
-    socket.send(JSON.stringify({ playerSegments }));
-
-    socket.on("message", (message) => {
-      const receivedData = JSON.parse(message);
-      // Update playerSegmentsOfPlayer2 with receivedData
-      playerSegmentsOfPlayer2 = receivedData.playerSegments;
-      // Call a function to update the other player's position
-      updatePlayer2Positions();
-    });
-  });
-
-  function updatePlayer2Positions() {
-    gridCells.forEach((cell) => cell.classList.remove("player2"));
-
-    playerSegmentsOfPlayer2.forEach((segment) => {
-      const segmentIndex = segment.y * gridSizeX + segment.x;
-      gridCells[segmentIndex].classList.add("player2");
-    });
-  }
-} else {
-  const socket = new WebSocket("ws://"+device2IP+":8080"); // Replace with Device 2's IP
+if (device1IP != null) {
+  const socket = new WebSocket("ws://" + device1IP + ":8080"); // Replace with Device 1/ host's IP
 
   socket.addEventListener("open", () => {
     console.log("Device 2: Connected to Device 1");
@@ -136,13 +107,14 @@ function updatePlayerPositions() {
 }
 
 function showGameOverCard() {
+  console.log("multi-game over");
   const gameOverCard = document.querySelector(".game-over-card");
   const playAgainButton = gameOverCard.querySelector(".play-again-button");
 
   gameOverCard.style.display = "block";
 
   playAgainButton.addEventListener("click", () => {
-    window.location.href = "../view/play_game.html";
+    window.location.href = "../view/multi_play_game.html";
   });
 }
 
